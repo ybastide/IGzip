1c5e8f13acdd359d0c578e449abf91b3a4bfe4d3
Yves Bastide <stid@acm.org>
2025-09-07 16:29:12 +0200

    🐛 fix: specify gzip mode
    Signed-off-by: Yves Bastide <stid@acm.org>

--- IGzip/IGZipBase.cs
+++ IGzip/IGZipBase.cs
@@ -53,6 +53,129 @@ internal static class IGZipBase
         IncorrectChecksum = -6
     }
 
+    /******************************************************************************/
+    /* Deflate Compression Standard Defines */
+    /******************************************************************************/
+    public const int IGZIP_K = 1024;
+    public const int DEF_MAX_HDR_SIZE = 328;
+    public const int DEF_MAX_CODE_LEN = 15;
+    public const int DEF_HIST_SIZE = 32 * IGZIP_K;
+    public const int DEF_MAX_HIST_BITS = 15;
+    public const int DEF_MAX_MATCH = 258;
+    public const int DEF_MIN_MATCH = 3;
+    public const int DEF_LIT_SYMBOLS = 257;
+    public const int DEF_LEN_SYMBOLS = 29;
+    public const int DEF_DIST_SYMBOLS = 30;
+    public const int DEF_LIT_LEN_SYMBOLS = DEF_LIT_SYMBOLS + DEF_LEN_SYMBOLS;
+
+    // Max repeat length, rounded up to 32 byte boundary
+    public const int LOOK_AHEAD = (DEF_MAX_MATCH + 31) & ~31;
+
+    // Deflate Implementation Specific Constants
+    // Note IGZIP_HIST_SIZE must be a power of two
+    public const int IGZIP_HIST_SIZE = DEF_HIST_SIZE;
+    public const bool LIMIT_HASH_UPDATE = true;
+    public const int IGZIP_HASH8K_HASH_SIZE = 8 * IGZIP_K;
+    public const int IGZIP_HASH_HIST_SIZE = IGZIP_HIST_SIZE;
+    public const int IGZIP_HASH_MAP_HASH_SIZE = IGZIP_HIST_SIZE;
+    public const int IGZIP_LVL0_HASH_SIZE = 8 * IGZIP_K;
+    public const int IGZIP_LVL1_HASH_SIZE = IGZIP_HASH8K_HASH_SIZE;
+    public const int IGZIP_LVL2_HASH_SIZE = IGZIP_HASH_HIST_SIZE;
+    public const int IGZIP_LVL3_HASH_SIZE = IGZIP_HASH_MAP_HASH_SIZE;
+
+#if LONGER_HUFFTABLE
+    public const int IGZIP_DIST_TABLE_SIZE = 8 * 1024;
+    public const int IGZIP_DECODE_OFFSET = 26;
+#else
+    public const int IGZIP_DIST_TABLE_SIZE = 2;
+    public const int IGZIP_DECODE_OFFSET = 0;
+#endif
+
+    public const int IGZIP_LEN_TABLE_SIZE = 256;
+    public const int IGZIP_LIT_TABLE_SIZE = DEF_LIT_SYMBOLS;
+    public const int IGZIP_HUFFTABLE_CUSTOM = 0;
+    public const int IGZIP_HUFFTABLE_DEFAULT = 1;
+    public const int IGZIP_HUFFTABLE_STATIC = 2;
+
+    public enum FlushFlags
+    {
+        NoFlush = 0, // Default
+        SyncFlush = 1,
+        FullFlush = 2,
+        FinishFlush = 0, // Deprecated
+    }
+
+    // Gzip Flags
+    public enum GzipFlags
+    {
+        Deflate = 0, // Default
+        Gzip = 1,
+        GzipNoHeader = 2,
+        Zlib = 3,
+        ZlibNoHeader = 4
+    }
+
+    /******************************************************************************/
+    /* Inflate Implementation Specific Defines */
+    /******************************************************************************/
+    const int DECODE_LONG_BITS = 12;
+    const int DECODE_SHORT_BITS = 10;
+
+    /* In the following defines, L stands for LARGE and S for SMALL */
+    const int L_REM = (21 - DECODE_LONG_BITS);
+    const int S_REM = (15 - DECODE_SHORT_BITS);
+
+    const int L_DUP = ((1 << L_REM) - (L_REM + 1));
+    const int S_DUP = ((1 << S_REM) - (S_REM + 1));
+
+    private const int L_UNUSED =
+        ((1 << L_REM) - (1 << ((L_REM) / 2)) - (1 << ((L_REM + 1) / 2)) + 1);
+    private const int S_UNUSED =
+        ((1 << S_REM) - (1 << ((S_REM) / 2)) - (1 << ((S_REM + 1) / 2)) + 1);
+
+
+    const int L_SIZE = (DEF_LIT_LEN_SYMBOLS + L_DUP + L_UNUSED);
+    const int S_SIZE = (DEF_DIST_SYMBOLS + S_DUP + S_UNUSED);
+
+    const int HUFF_CODE_LARGE_LONG_ALIGNED = (L_SIZE + (-L_SIZE & 0xf));
+    const int HUFF_CODE_SMALL_LONG_ALIGNED = (S_SIZE + (-S_SIZE & 0xf));
+
+
+    /** @brief Large lookup table for decoding huffman codes */
+    [StructLayout(LayoutKind.Sequential)]
+    unsafe struct inflate_huff_code_large
+    {
+        fixed uint short_code_lookup[1 << (DECODE_LONG_BITS)]; //!< Short code lookup table
+        fixed ushort long_code_lookup[HUFF_CODE_LARGE_LONG_ALIGNED]; //!< Long code lookup table
+    };
+
+    /** @brief Small lookup table for decoding huffman codes */
+    [StructLayout(LayoutKind.Sequential)]
+    unsafe struct inflate_huff_code_small
+    {
+        fixed ushort short_code_lookup[1 << (DECODE_SHORT_BITS)]; //!< Short code lookup table
+        fixed ushort long_code_lookup[HUFF_CODE_SMALL_LONG_ALIGNED]; //!< Long code lookup table
+    };
+
+    /* Current state of decompression */
+    enum block_state
+    {
+        BLOCK_NEW_HDR, /* Just starting a new block */
+        BLOCK_HDR, /* In the middle of reading in a block header */
+        BLOCK_TYPE0, /* Decoding a type 0 block */
+        BLOCK_CODED, /* Decoding a huffman coded block */
+        BLOCK_INPUT_DONE, /* Decompression of input is completed */
+        BLOCK_FINISH, /* Decompression of input is completed and all data has been flushed to
+                              output */
+        GZIP_EXTRA_LEN,
+        GZIP_EXTRA,
+        GZIP_NAME,
+        GZIP_COMMENT,
+        GZIP_HCRC,
+        ZLIB_DICT,
+        CHECKSUM_CHECK,
+    };
+
     [StructLayout(LayoutKind.Sequential)]
     internal struct InflateStateStart
     {
@@ -60,9 +183,19 @@ internal static class IGZipBase
         public int avail_out;
         public int total_out;
         public unsafe byte* next_in;
-        public ulong total_in;
+        public ulong read_in;
         public int avail_in;
-        // the other fields are not used
+        int read_in_length; //!< Bits in read_in
+        inflate_huff_code_large lit_huff_code; //!< Structure for decoding lit/len symbols
+
+        inflate_huff_code_small dist_huff_code; //!< Structure for decoding dist symbols
+
+        block_state block_state; //!< Current decompression state
+
+        uint dict_length; //!< Length of dictionary used
+        uint bfinal; //!< Flag identifying final block
+        public GzipFlags crc_flag; //!< Flag identifying whether to track of crc
+        public uint crc; //!< Contains crc or adler32 of output if crc_flag is set
     }
 
     private static string? _realLibraryName;
--- IGzip/IGzip.cs
+++ IGzip/IGzip.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace IGzip;
 
 public static class IGzip
@@ -6,7 +8,12 @@ public static class IGzip
 
     public static int Inflate(ReadOnlySpan<byte> input, byte[] output, int offset = 0)
     {
-        int total = 0;
+        if (Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "crc_flag") != 21172)
+        {
+            throw new Exception("Offset of crc_flag is not 21172");
+        }
+
+        int total;
         unsafe
         {
             byte[] streamSpace = new byte[IGZipBase.InflateStateStructSize];
@@ -15,13 +22,22 @@ public static class IGzip
             fixed (byte* pOutput = output)
             {
                 IGZipBase.InflateStateStart* state = (IGZipBase.InflateStateStart*)pStreamSpace;
+                IGZipBase.InflateInit(state);
 
                 state->avail_in = input.Length;
                 state->next_in = pInput;
                 state->avail_out = output.Length - offset;
                 state->next_out = pOutput + offset;
+                state->crc_flag = IGZipBase.GzipFlags.Gzip;
+                Console.WriteLine($"next_out: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "next_out")}");
+                Console.WriteLine($"avail_out: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "avail_out")}");
+                Console.WriteLine($"total_out: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "total_out")}");
+                Console.WriteLine($"next_in: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "next_in")}");
+                Console.WriteLine($"read_in: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "read_in")}");
+                Console.WriteLine($"avail_in: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "avail_in")}");
+                Console.WriteLine($"crc_flag: {Marshal.OffsetOf(typeof(IGZipBase.InflateStateStart), "crc_flag")}");
                 var result = (IGZipBase.DecompResult)IGZipBase.Inflate(state);
-                if (result != IGZipBase.DecompResult.DecompOk && result != IGZipBase.DecompResult.EndInput)
+                if (result != IGZipBase.DecompResult.DecompOk /*&& result != IGZipBase.DecompResult.EndInput*/)
                     throw new Exception($"Decompression failed with error code {result}");
                 total = state->total_out;
             }
